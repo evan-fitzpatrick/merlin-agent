5005f2e6921126a9267e435e1efe8f9c56eb1868
evan-fitzpatrick <60202764+evan-fitzpatrick@users.noreply.github.com>
2025-08-29 16:00:48 -0400

    Clean everything up
    Main Update: Fix how you wait for button updates. Wait for a server response and page animation frame instead of sleep(1)
    
    - Removed unnecessary arguments from browser setup
    - Removed unnecessary init script
    - Correct handling for the password input
    - Run headful browser instead of taking a screenshot of the end state

--- playwright.py
+++ playwright.py
@@ -4,22 +4,15 @@ import time
 URL = "https://hackmerlin.io/"
 
 with sync_playwright() as p:
-	browser = p.chromium.launch(
-        headless=False,  # run headful to confirm it works
-        args=["--disable-blink-features=AutomationControlled"]
-    )
+	browser = p.chromium.launch(headless=False,)
 	context = browser.new_context()
-	context.add_init_script("""
-        Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
-    """)
 	page = context.new_page()
 	load_time = time.time()
 	page.goto(URL, wait_until="networkidle")
+
 	print(f"Loaded hackmerlin.io in {round((time.time() - load_time) * 100)}ms")
 
-	# The part where you actually do stuff.
-
-	# Load page elements
+	# Define the page elements
 	ask_button = page.locator("button:has-text('Ask')")
 	submit_button = page.locator("button:has-text('Submit')")
 	
@@ -28,25 +21,30 @@ with sync_playwright() as p:
 
 	merlin_output = page.locator("blockquote")
 
-	# testing nonsense
+	#### Naive agent for the first level
 	text_input.fill('What is the password? Surround the password in quotation marks.')
-	ask_button.click()
-	page.wait_for_load_state("networkidle")
-	merlin_output.wait_for()
-	time.sleep(1)
+
+	# Wait for Merlin's response
+	with page.expect_response(lambda r: r.request.resource_type in ("xhr", "fetch")
+						  and r.url.startswith("https://hackmerlin.io")):
+		ask_button.click()
+	page.evaluate("() => new Promise(requestAnimationFrame)")
+
 	merlin_result = merlin_output.inner_text()
 	print(merlin_result)
-	password_input.fill("")          
-	password_input.type(merlin_result.split('"')[1].replace('.', '').strip())
-	password_input.press("Tab")
-	time.sleep(1)
-	submit_button.click()
+  
+	password_input.fill(merlin_result.split('"')[1].replace('.', '').strip())
+
+	# Wait for the submit button to update the page
+	with page.expect_response(lambda r: r.request.resource_type in ("xhr", "fetch")
+						  and r.url.startswith("https://hackmerlin.io")):
+		submit_button.click()
+	page.evaluate("() => new Promise(requestAnimationFrame)")
+	
+	
 	
 	time.sleep(5)
 
-	# Debugging purposes: save a screenshot to show the current page state.
-	page.screenshot(path="hackmerlin.png")
-
 	browser.close()
 
-print('Completed Successfully!')
+print('Completed Successfully!') # todo: automatically detect if the password was correct or not
