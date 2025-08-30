50b9ca88d2e0dda294d08bb137cfb87250844c60
evan-fitzpatrick <60202764+evan-fitzpatrick@users.noreply.github.com>
2025-08-30 16:25:27 -0400

    Update agent.py
    change default model to kimi k2 (much faster)
    fix bug if model cant solve earlier levels.

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ import time
 
 system_prompt = open('sysprompt.txt', 'r').read()
 api_key = open('openrouterkey-1.txt', 'r').read().strip()
-model = "deepseek/deepseek-r1-0528:free"
+model = "moonshotai/kimi-k2:free"
 
 
 merlin = Merlin(headless=False)
@@ -14,36 +14,36 @@ wrapper = OpenrouterWrapper(
 	system_prompt=system_prompt
 )
 
-max_messages = 10 # You may want this to be higher for level 7
 
-# Dialogue loop
-for level in range(1,8):
-	# We clear the context for each new level
-	merlin_message = 'Hello traveler! Ask me anything...'
-	wrapper.clear_history(keep_system=True)
-	bad_password = False
-	print(f'Trying to solve level {level}')
+def play_game(model, system_prompt, api_key, max_messages = 10): # You may want max_messages to be higher for level 7
+	for level in range(1,8):
+		# We clear the context for each new level
+		merlin_message = 'Hello traveler! Ask me anything...'
+		wrapper.clear_history(keep_system=True)
+		bad_password = False
+		print(f'Trying to solve level {level}')
 
-	# Dialogue loop
-	for i in range(max_messages):
-		agent_message = wrapper.send_message(merlin_message)
+		# Dialogue loop
+		for i in range(max_messages):
+			agent_message = wrapper.send_message(merlin_message)
 
-		# Basic tool call
-		if 'Merlin:' in agent_message:
-			merlin_message = merlin.message(agent_message.split('Merlin: ')[-1])
-		elif 'Password:' in agent_message:
-			password_result = merlin.guess_password(agent_message.split('Password: ')[-1])
-			if password_result:
-				print(f'Successfully Completed Level {level}!\n')
-				break
+			# Basic tool call
+			if 'Merlin:' in agent_message:
+				merlin_message = merlin.message(agent_message.split('Merlin: ')[-1])
+			elif 'Password:' in agent_message:
+				password_result = merlin.guess_password(agent_message.split('Password: ')[-1])
+				if password_result:
+					print(f'Successfully Completed Level {level}!\n')
+					break
+				else:
+					merlin_message = 'Alas! That password is incorrect!'
 			else:
-				merlin_message = 'Alas! That password is incorrect!'
-		else:
-			print(f'!! Invalid Tool Call !! \n{agent_message}\n')
+				print(f'!! Invalid Tool Call !! \n{agent_message}\n')
 
-		# Abort if max messages is reached
-		if i + 1 == max_messages:
-			print(f'{model} got to level {level}.')
-			break;break
+			# Abort if max messages is reached
+			if i + 1 == max_messages:
+				print(f'{model} got to level {level}.')
+				return
 
+play_game(model, system_prompt, api_key, max_messages=25)
 merlin.close() 
